52257002aa3579242a881232ffb36f9bc4bad6b2
Nicola <nicola.ba1989@gmail.com>
2020-04-02 07:54:14 +0200

    up add tags guest index

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -10,6 +10,7 @@
                             <div class="card-body">
                                 <h5 class="card-title text-center">{{$post->title}}</h5>
                                 <p class="card-text">{{$post->body}}</p>
+                                <p class="card-text">@foreach ($post->tags as $tag) <a href="#">#{{$tag->name}}</a> @endforeach</p>
                             </div>
                             <div class="card-footer text-muted d-flex justify-content-between align-items-center">
                                 <p class="card-text m-0">{{$post->user->name}} il {{$post->created_at}}</p>
